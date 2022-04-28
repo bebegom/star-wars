983b05acee3dd2b3e44b01cd73bde2d70b8fc5b3
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-04-28 12:15:32 +0200

    now showing the attributes on detail-page

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,7 +1,10 @@
 import { React, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import API from '../services/StarwarsAPI'
+// import API from '../services/StarwarsAPI'
 import axios from 'axios'
+//TODO: for looping over characters in the film
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+// import { Link } from 'react-router-dom'
 
 axios.defaults.baseURL = 'https://swapi.dev/api'
 
@@ -10,25 +13,60 @@ const FilmPage = () => {
 
     const { id } = useParams()
 
-    const getFilmFromAPI = async (theParams) => {
+    const getFilm = async (theParams) => {
         const urlWithId = await axios.get(`/films/${theParams}`)
         setFilm(urlWithId.data)
     }
 
     useEffect( ()=> {
-        getFilmFromAPI(id)
+        getFilm(id)
     }, [id])
 
     return (
         <div>
-            <h1>One film</h1>
-
             {!film && (
                 <p>wait for it...</p>
             )}
 
             {film && (
-                <p>i got a film: {film.title}</p>
+                <div className='border border-dark'>
+                    <h1 className='detail-header'>Title of film: {film.title}</h1>
+
+                    <h2>Attributes:</h2>
+
+                    <table>
+                        <tr>
+                            <th>Episode</th>
+                            <td>{film.episode_id}</td>
+                        </tr>
+                        <tr>
+                            <th>Director</th>
+                            <td>{film.director}</td>
+                        </tr>
+                        <tr>
+                            <th>Producer</th>
+                            <td>{film.producer}</td>
+                        </tr>
+                        <tr>
+                            <th>Release date</th>
+                            <td>{film.release_date}</td>
+                        </tr>
+                        <tr>
+                            <th>Characters</th>
+                            <td>{film.characters}</td>
+                        </tr>
+
+                    </table>
+
+                    <h2>Characters</h2>
+
+                    {/* <ListGroup>
+                        {film.characters.map(character => (
+                            <ListGroupItem>{character.name}</ListGroupItem>
+                        ))}
+                    </ListGroup> */}
+                </div>
+
             )}
         </div>
     )
